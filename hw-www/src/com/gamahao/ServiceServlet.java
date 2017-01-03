d3aa4cc4bd09b2c399f3d842416246e93fcbe86a
Gama <rs556655@gmail.com>
2017-01-04 01:48:07 +0800

    修改 Servlet

--- hw-www/src/com/gamahao/ServiceServlet.java
+++ hw-www/src/com/gamahao/ServiceServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import ticket.GetAvailableSeats;
+import ticket.GetDestination;
 
 public class ServiceServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -49,7 +50,31 @@ public class ServiceServlet extends HttpServlet {
 	private void serviceForward(String service, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
 		
 		if (service.equals("getAvailableSeats"))
-			response.getWriter().print(GetAvailableSeats.getSeats(request.getParameter("car_name"), request.getParameter("date"), request.getParameter("begin"), request.getParameter("end")));
+			response.getWriter().print(
+					GetAvailableSeats.getSeats(
+							request.getParameter("car_name"), 
+							request.getParameter("date"), 
+							request.getParameter("begin"), 
+							request.getParameter("end")));
+		
+		else if (service.equals("getDestination"))
+			response.getWriter().print(
+					GetDestination.getDestination(
+							request.getParameter("route"), 
+							request.getParameter("start")));
+		
+		else if (service.equals("getStation"))
+			response.getWriter().print(
+					GetStation.getStation(
+							request.getParameter("route")));
+		
+		else if (service.equals("getCar"))
+			response.getWriter().print(
+					GetCar.getCar(
+							request.getParameter("route"), 
+							request.getParameter("start"), 
+							request.getParameter("date"), 
+							request.getParameter("destination")));
 		else
 			response.sendError(HttpServletResponse.SC_NOT_FOUND);
 		
--- hw-www/src/ticket/HeaderCreater.java
+++ hw-www/src/ticket/HeaderCreater.java
@@ -2,33 +2,35 @@ package ticket;
 
 public class HeaderCreater {
 	public static String createHeader(String currentPage) {
-			if (currentPage == null) currentPage = "";
-		
-			StringBuilder sb = new StringBuilder();
-			sb.append("<link href=\"/css/header.css\" rel=\"stylesheet\" />");
-			sb.append("<header>");
-			sb.append("<nav><div class=\"nav-wrapper grey darken-4\">" + "<a href=\"/\" class=\"brand-logo center\"> "
-					+ "<i class=\"material-icons\">&nbsp loyalty</i>屍速客運訂票系統</a>" + "</div>"
-					+ "<div class=\"nav-wrapper grey darken-4\">");
+		if (currentPage == null)
+			currentPage = "";
 
-			if (currentPage.equals("agreement")) 
-				sb.append("<div class=\"center\" id=\"crumbs\">	" + "<ul><li><a class=\"active\">條款</a></li>"
-						+ "<li><a>填寫資料</a></li>" + "<li><a>選擇車次</a></li>" + "<li><a>結果</a></li></ul>" + "</div>");
-			else if (currentPage.equals("form")) 
-				sb.append("<div class=\"center\" id=\"crumbs\">" + "<ul><li><a class=\"active\">條款</a></li>"
-						+ "<li><a class=\"active\">填寫資料</a></li>" + "<li><a>選擇車次</a></li>" + "<li><a>結果</a></li></ul>"
-						+ "</div>");
-			else if (currentPage.equals("reservation")) 
-				sb.append("<div class=\"center\" id=\"crumbs\">	" + "<ul><li><a class=\"active\">條款</a></li>"
-						+ "<li><a class=\"active\">填寫資料</a></li>" + "<li><a class=\"active\">選擇車次</a></li>"
-						+ "<li><a>結果</a></li></ul>" + "</div>");
-			else if (currentPage.equals("result")) 
-				sb.append("<div class=\"center\" id=\"crumbs\">	" + "<ul><li><a class=\"active\">條款</a></li>"
-						+ "<li><a class=\"active\">填寫資料</a></li>" + "<li><a class=\"active\">選擇車次</a></li>"
-						+ "<li><a class=\"active\">結果</a></li></ul>" + "</div>");
-			else sb.append("<div class=\"center\" id=\"current_page\">" + currentPage + "</div>");
-			
-			sb.append("</div><div  style=\"height:65px;\"></nav></header>");
-			return sb.toString();
-		}
+		StringBuilder sb = new StringBuilder();
+		sb.append("<link href=\"/css/header.css\" rel=\"stylesheet\" />");
+		sb.append("<header>");
+		sb.append("<nav><div class=\"nav-wrapper grey darken-4\">" + "<a href=\"/\" class=\"brand-logo center\"> "
+				+ "<i class=\"material-icons\">&nbsp loyalty</i>屍速客運訂票系統</a>" + "</div>"
+				+ "<div class=\"nav-wrapper grey darken-4\">");
+
+		if (currentPage.equals("agreement"))
+			sb.append("<div class=\"center\" id=\"crumbs\">	" + "<ul><li><a class=\"active\">條款</a></li>"
+					+ "<li><a>填寫資料</a></li>" + "<li><a>選擇車次</a></li>" + "<li><a>結果</a></li></ul>" + "</div>");
+		else if (currentPage.equals("form"))
+			sb.append("<div class=\"center\" id=\"crumbs\">" + "<ul><li><a class=\"active\">條款</a></li>"
+					+ "<li><a class=\"active\">填寫資料</a></li>" + "<li><a>選擇車次</a></li>" + "<li><a>結果</a></li></ul>"
+					+ "</div>");
+		else if (currentPage.equals("reservation"))
+			sb.append("<div class=\"center\" id=\"crumbs\">	" + "<ul><li><a class=\"active\">條款</a></li>"
+					+ "<li><a class=\"active\">填寫資料</a></li>" + "<li><a class=\"active\">選擇車次</a></li>"
+					+ "<li><a>結果</a></li></ul>" + "</div>");
+		else if (currentPage.equals("result"))
+			sb.append("<div class=\"center\" id=\"crumbs\">	" + "<ul><li><a class=\"active\">條款</a></li>"
+					+ "<li><a class=\"active\">填寫資料</a></li>" + "<li><a class=\"active\">選擇車次</a></li>"
+					+ "<li><a class=\"active\">結果</a></li></ul>" + "</div>");
+		else
+			sb.append("<div class=\"center\" id=\"current_page\">" + currentPage + "</div>");
+
+		sb.append("</div><div  style=\"height:65px;\"></nav></header>");
+		return sb.toString();
+	}
 }
