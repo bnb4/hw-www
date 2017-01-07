0531f26aba2a3a11d373a9e2b7e5b510b2a278d4
Wang <david1234012@gmail.com>
2017-01-07 23:41:58 +0800

    新增GetTicket

--- hw-www/src/ticket/GetTicket.java
+++ hw-www/src/ticket/GetTicket.java
@@ -2,4 +2,9 @@ package ticket;
 
 public class GetTicket {
 
+	public static boolean getTicket(String parameter, String parameter2, String parameter3, String parameter4) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
