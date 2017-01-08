c7d24435eac6aca67fb35a0cb888c12e0c333a57
Wang <david1234012@gmail.com>
2017-01-08 20:54:28 +0800

    修正GetTicket參數

--- hw-www/src/ticket/GetTicket.java
+++ hw-www/src/ticket/GetTicket.java
@@ -4,8 +4,8 @@ import javax.servlet.http.HttpSession;
 
 public class GetTicket {
 
-	public static boolean getTicket(HttpSession session, String parameter, String parameter2, String parameter3, String parameter4) {
-		// TODO Auto-generated method stub
+	public static boolean getTicket(HttpSession session, String start, String end, String date, String car_name) {
+		
 		return false;
 	}
 
